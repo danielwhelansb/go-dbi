af69a0ee59a5da753b5d0155e7477fe8898cd7e0
Thomas Lee <me@tomlee.co>
2010-12-06 23:04:41 +1100

    Add GetOne method to the Connection interface.

--- connection.go
+++ connection.go
@@ -3,6 +3,7 @@ package dbi
 import "os"
 
 type Connection interface {
+    GetOne(sql string, params ...interface{}) (interface{}, os.Error)
     Close() os.Error
 }
 
--- connection_test.go
+++ connection_test.go
@@ -13,14 +13,36 @@ type DummyConnection struct {
     options map[string]interface{}
 }
 
+func (self *DummyConnection) GetOne(sql string, params ...interface{}) (interface{}, os.Error) {
+    return 42, nil
+}
+
 func (self *DummyConnection) Close() os.Error {
     return nil
 }
+
+//
+// Below we test everything via the Connection interface to ensure that
+// the method really needs to be provided by DummyConnection.
+//
 
 func TestCloseMethodWorks(*testing.T) {
     conn := new(DummyConnection)
     func(c Connection) {
         c.Close()
+    }(conn)
+}
+
+func TestGetOneMethodWorks(t *testing.T) {
+    conn := new(DummyConnection)
+    func(c Connection) {
+        res, err := c.GetOne("SELECT * FROM foo")
+        if err != nil {
+            t.Fatal("Expected GetOne() to pass")
+        }
+        if res.(int) != 42 {
+            t.Fatal("Expected result == 42")
+        }
     }(conn)
 }
 
