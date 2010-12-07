59c5fa33049baccf96555319f46a17e337a086a2
Thomas Lee <me@tomlee.co>
2010-12-07 20:16:10 +1100

    Simplify the ResultSet interface at the expense of a little type safety.

--- result_set.go
+++ result_set.go
@@ -10,17 +10,11 @@ type ResultSet interface {
     // Get the next row of results.
     Next() bool
 
-    // Get a string value by column index (zero-based).
-    GetStringByCol(col int) (string, os.Error)
+    // Get a string value by column name or zero-based index.
+    GetString(col interface{}) (string, os.Error)
 
-    // Get a string value by column name.
-    GetString(col string) (string, os.Error)
-
-    // Get an arbitrary value by column index (zero-based).
-    GetValueByCol(col int) (interface{}, os.Error)
-
-    // Get an arbitrary value by column name.
-    GetValue(col string) (interface{}, os.Error)
+    // Get a value of arbitrary type by column name or zero-based index.
+    GetValue(col interface{}) (interface{}, os.Error)
 
     // Release the resources held by a ResultSet.
     Close() os.Error
--- result_set_test.go
+++ result_set_test.go
@@ -16,20 +16,12 @@ func (self *DummyResultSet) Next() bool {
     return false
 }
 
-func (self *DummyResultSet) GetStringByCol(col int) (string, os.Error) {
+func (self *DummyResultSet) GetString(col interface{}) (string, os.Error) {
     return "hello", nil
 }
 
-func (self *DummyResultSet) GetString(col string) (string, os.Error) {
-    return "hello with string", nil
-}
-
-func (self *DummyResultSet) GetValueByCol(col int) (interface{}, os.Error) {
+func (self *DummyResultSet) GetValue(col interface{}) (interface{}, os.Error) {
     return 123, nil
-}
-
-func (self *DummyResultSet) GetValue(col string) (interface{}, os.Error) {
-    return 456, nil
 }
 
 func (self *DummyResultSet) Close() os.Error {
@@ -48,27 +40,17 @@ func TestResultSetNextMethodWorks(t *testing.T) {
     }(new(DummyResultSet))
 }
 
-func TestResultSetGetStringByColMethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.GetStringByCol(1)
-    }(new(DummyResultSet))
-}
-
 func TestResultSetGetStringMethodWorks(t *testing.T) {
     func(rs ResultSet) {
+        rs.GetString(1)
         rs.GetString("foo")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetValueByColMethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.GetValueByCol(0)
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetValueMethodWorks(t *testing.T) {
     func(rs ResultSet) {
         rs.GetValue("test")
+        rs.GetValue(0)
     }(new(DummyResultSet))
 }
 
