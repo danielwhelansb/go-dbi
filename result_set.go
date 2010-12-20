e193ea6559b311e05161632a2b2bcd718273d7ec
Thomas Lee <me@tomlee.co>
2010-12-20 19:33:13 +1100

    Remove 'Get' prefix from accessors as suggested by Russ Cox.

--- result_set.go
+++ result_set.go
@@ -11,40 +11,40 @@ type ResultSet interface {
     Next() bool
 
     // Get a string value by column name or zero-based index.
-    GetString(col interface{}) (string, os.Error)
+    String(col interface{}) (string, os.Error)
 
     // Get an int8 value by column name or zero-based index.
-    GetInt8(col interface{}) (int8, os.Error)
+    Int8(col interface{}) (int8, os.Error)
 
     // Get an int16 value by column name or zero-based index.
-    GetInt16(col interface{}) (int16, os.Error)
+    Int16(col interface{}) (int16, os.Error)
 
     // Get an int32 value by column name or zero-based index.
-    GetInt32(col interface{}) (int32, os.Error)
+    Int32(col interface{}) (int32, os.Error)
 
     // Get an int64 value by column name or zero-based index.
-    GetInt64(col interface{}) (int64, os.Error)
+    Int64(col interface{}) (int64, os.Error)
 
     // Get a uint8/byte value by column name or zero-based index.
-    GetUint8(col interface{}) (uint8, os.Error)
+    Uint8(col interface{}) (uint8, os.Error)
 
     // Get a uint16 value by column name or zero-based index.
-    GetUint16(col interface{}) (uint16, os.Error)
+    Uint16(col interface{}) (uint16, os.Error)
 
     // Get a uint32 value by column name or zero-based index.
-    GetUint32(col interface{}) (uint32, os.Error)
+    Uint32(col interface{}) (uint32, os.Error)
 
     // Get a uint64 value by column name or zero-based index.
-    GetUint64(col interface{}) (uint64, os.Error)
+    Uint64(col interface{}) (uint64, os.Error)
 
     // Get a float32 value by column name or zero-based index.
-    GetFloat32(col interface{}) (float32, os.Error)
+    Float32(col interface{}) (float32, os.Error)
 
     // Get a float64 value by column name or zero-based index.
-    GetFloat64(col interface{}) (float64, os.Error)
+    Float64(col interface{}) (float64, os.Error)
 
     // Get a value of arbitrary type by column name or zero-based index.
-    GetValue(col interface{}) (interface{}, os.Error)
+    Value(col interface{}) (interface{}, os.Error)
 
     // Release the resources held by a ResultSet.
     Close() os.Error
--- result_set_test.go
+++ result_set_test.go
@@ -16,51 +16,51 @@ func (self *DummyResultSet) Next() bool {
     return false
 }
 
-func (self *DummyResultSet) GetString(col interface{}) (string, os.Error) {
+func (self *DummyResultSet) String(col interface{}) (string, os.Error) {
     return "hello", nil
 }
 
-func (self *DummyResultSet) GetInt8(col interface{}) (int8, os.Error) {
+func (self *DummyResultSet) Int8(col interface{}) (int8, os.Error) {
     return 127, nil
 }
 
-func (self *DummyResultSet) GetInt16(col interface{}) (int16, os.Error) {
+func (self *DummyResultSet) Int16(col interface{}) (int16, os.Error) {
     return 12345, nil
 }
 
-func (self *DummyResultSet) GetInt32(col interface{}) (int32, os.Error) {
+func (self *DummyResultSet) Int32(col interface{}) (int32, os.Error) {
     return 1, nil
 }
 
-func (self *DummyResultSet) GetInt64(col interface{}) (int64, os.Error) {
+func (self *DummyResultSet) Int64(col interface{}) (int64, os.Error) {
     return 1, nil
 }
 
-func (self *DummyResultSet) GetUint8(col interface{}) (uint8, os.Error) {
+func (self *DummyResultSet) Uint8(col interface{}) (uint8, os.Error) {
     return 1, nil
 }
 
-func (self *DummyResultSet) GetUint16(col interface{}) (uint16, os.Error) {
+func (self *DummyResultSet) Uint16(col interface{}) (uint16, os.Error) {
     return 65535, nil
 }
 
-func (self *DummyResultSet) GetUint32(col interface{}) (uint32, os.Error) {
+func (self *DummyResultSet) Uint32(col interface{}) (uint32, os.Error) {
     return 1, nil
 }
 
-func (self *DummyResultSet) GetUint64(col interface{}) (uint64, os.Error) {
+func (self *DummyResultSet) Uint64(col interface{}) (uint64, os.Error) {
     return 1, nil
 }
 
-func (self *DummyResultSet) GetFloat32(col interface{}) (float32, os.Error) {
+func (self *DummyResultSet) Float32(col interface{}) (float32, os.Error) {
     return 0, nil
 }
 
-func (self *DummyResultSet) GetFloat64(col interface{}) (float64, os.Error) {
+func (self *DummyResultSet) Float64(col interface{}) (float64, os.Error) {
     return 0, nil
 }
 
-func (self *DummyResultSet) GetValue(col interface{}) (interface{}, os.Error) {
+func (self *DummyResultSet) Value(col interface{}) (interface{}, os.Error) {
     return 123, nil
 }
 
@@ -84,85 +84,85 @@ func TestResultSetNextMethodWorks(t *testing.T) {
 
 func TestResultSetGetStringMethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetString(1)
-        rs.GetString("foo")
+        rs.String(1)
+        rs.String("foo")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetInt8MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetInt8(0)
-        rs.GetInt8("blort")
+        rs.Int8(0)
+        rs.Int8("blort")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetInt16MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetInt16(0)
-        rs.GetInt16("snert")
+        rs.Int16(0)
+        rs.Int16("snert")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetInt32MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetInt32(1)
-        rs.GetInt32("bar")
+        rs.Int32(1)
+        rs.Int32("bar")
     }(new(DummyResultSet));
 }
 
 func TestResultSetGetInt64MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetInt64(1)
-        rs.GetInt64("baz")
+        rs.Int64(1)
+        rs.Int64("baz")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetUint8MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetUint8(0)
-        rs.GetUint8("bleh")
+        rs.Uint8(0)
+        rs.Uint8("bleh")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetUint16MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetUint16(1)
-        rs.GetUint16("bort")
+        rs.Uint16(1)
+        rs.Uint16("bort")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetUint32MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetUint32(1)
-        rs.GetUint32("boz")
+        rs.Uint32(1)
+        rs.Uint32("boz")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetUint64MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetUint64(1)
-        rs.GetUint64("bleh")
+        rs.Uint64(1)
+        rs.Uint64("bleh")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetFloat32MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetFloat32(8)
-        rs.GetFloat32("moo")
+        rs.Float32(8)
+        rs.Float32("moo")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetFloat64MethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetFloat64(2)
-        rs.GetFloat64("bah")
+        rs.Float64(2)
+        rs.Float64("bah")
     }(new(DummyResultSet))
 }
 
 func TestResultSetGetValueMethodWorks(t *testing.T) {
     func(rs ResultSet) {
-        rs.GetValue("test")
-        rs.GetValue(0)
+        rs.Value("test")
+        rs.Value(0)
     }(new(DummyResultSet))
 }
 
