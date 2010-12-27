201e83c1e6db79a448c10bbd5bb69d6eb0344228
Thomas Lee <me@tomlee.co>
2010-12-27 15:40:28 +1100

    Remove accessor methods (favour Scan/NamedScan)

--- result_set.go
+++ result_set.go
@@ -16,42 +16,6 @@ type ResultSet interface {
     // Scan the current row of results by column name.
     NamedScan(refs ...interface{}) os.Error
 
-    // Get a string value by column name.
-    String(name string) (string, os.Error)
-
-    // Get an int8 value by column name.
-    Int8(col string) (int8, os.Error)
-
-    // Get an int16 value by column name.
-    Int16(col string) (int16, os.Error)
-
-    // Get an int32 value by column name.
-    Int32(col string) (int32, os.Error)
-
-    // Get an int64 value by column name.
-    Int64(col string) (int64, os.Error)
-
-    // Get a uint8/byte value by column name.
-    Uint8(col string) (uint8, os.Error)
-
-    // Get a uint16 value by column name.
-    Uint16(col string) (uint16, os.Error)
-
-    // Get a uint32 value by column name.
-    Uint32(col string) (uint32, os.Error)
-
-    // Get a uint64 value by column name.
-    Uint64(col string) (uint64, os.Error)
-
-    // Get a float32 value by column name.
-    Float32(col string) (float32, os.Error)
-
-    // Get a float64 value by column name.
-    Float64(col string) (float64, os.Error)
-
-    // Get a value of arbitrary type by column name.
-    Value(col string) (interface{}, os.Error)
-
     // Release the resources held by a ResultSet.
     Close() os.Error
 }
--- result_set_test.go
+++ result_set_test.go
@@ -24,54 +24,6 @@ func (self *DummyResultSet) NamedScan(refs ...interface{}) os.Error {
     return nil
 }
 
-func (self *DummyResultSet) String(col string) (string, os.Error) {
-    return "hello", nil
-}
-
-func (self *DummyResultSet) Int8(col string) (int8, os.Error) {
-    return 127, nil
-}
-
-func (self *DummyResultSet) Int16(col string) (int16, os.Error) {
-    return 12345, nil
-}
-
-func (self *DummyResultSet) Int32(col string) (int32, os.Error) {
-    return 1, nil
-}
-
-func (self *DummyResultSet) Int64(col string) (int64, os.Error) {
-    return 1, nil
-}
-
-func (self *DummyResultSet) Uint8(col string) (uint8, os.Error) {
-    return 1, nil
-}
-
-func (self *DummyResultSet) Uint16(col string) (uint16, os.Error) {
-    return 65535, nil
-}
-
-func (self *DummyResultSet) Uint32(col string) (uint32, os.Error) {
-    return 1, nil
-}
-
-func (self *DummyResultSet) Uint64(col string) (uint64, os.Error) {
-    return 1, nil
-}
-
-func (self *DummyResultSet) Float32(col string) (float32, os.Error) {
-    return 0, nil
-}
-
-func (self *DummyResultSet) Float64(col string) (float64, os.Error) {
-    return 0, nil
-}
-
-func (self *DummyResultSet) Value(col string) (interface{}, os.Error) {
-    return 123, nil
-}
-
 func (self *DummyResultSet) Close() os.Error {
     return nil
 }
@@ -101,78 +53,6 @@ func TestResultSetNamedScanMethodWorks(t *testing.T) {
     func(rs ResultSet) {
         var foo string
         rs.NamedScan("foo", &foo)
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetStringMethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.String("foo")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetInt8MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Int8("blort")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetInt16MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Int16("snert")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetInt32MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Int32("bar")
-    }(new(DummyResultSet));
-}
-
-func TestResultSetGetInt64MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Int64("baz")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetUint8MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Uint8("bleh")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetUint16MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Uint16("bort")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetUint32MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Uint32("boz")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetUint64MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Uint64("bleh")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetFloat32MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Float32("moo")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetFloat64MethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Float64("bah")
-    }(new(DummyResultSet))
-}
-
-func TestResultSetGetValueMethodWorks(t *testing.T) {
-    func(rs ResultSet) {
-        rs.Value("test")
     }(new(DummyResultSet))
 }
 
